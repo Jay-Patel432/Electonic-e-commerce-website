409ef75a17840b875e4d90bdfd0d2241d596a284
Jay-Patel432 <155234969+Jay-Patel432@users.noreply.github.com>
2023-12-30 19:26:50 +0530

    Add files via upload

---
+++ project/boxes/woofer.js
@@ -0,0 +1,47 @@
+function linkds(){
+    document.getElementById("Specification").style.
+    display="none";
+    document.getElementById("Description").style.
+    display="block";
+    document.getElementById('Reviews').style.display="none";
+    document.getElementById('video').style.display="none";
+}
+function linksp(){
+    document.getElementById("Description").style.
+    display="none";
+    document.getElementById("Specification").style.
+    display="block";
+    document.getElementById('Reviews').style.display="none";
+    document.getElementById('video').style.display="none";
+}
+function linkre(){
+    document.getElementById("Description").style.
+    display="none";
+    document.getElementById("Specification").style.
+    display="none";
+    document.getElementById('Reviews').style.display="block";
+    document.getElementById('video').style.display="none";
+}
+function linkvi(){
+    document.getElementById("Description").style.
+    display="none";
+    document.getElementById("Specification").style.
+    display="none";
+    document.getElementById('Reviews').style.display="none";
+    document.getElementById('video').style.display="block";
+}
+function woof1(){
+    document.getElementById("latest1").style.display="block";
+    document.getElementById("latest2").style.display="none";
+    document.getElementById("latest3").style.display="none";
+}
+function woof2(){
+    document.getElementById("latest1").style.display="none";
+    document.getElementById("latest2").style.display="block";
+    document.getElementById("latest3").style.display="none";
+}
+function woof3(){
+    document.getElementById("latest1").style.display="none";
+    document.getElementById("latest2").style.display="none";
+    document.getElementById("latest3").style.display="block";
+}
---
+++ project/main.js
@@ -0,0 +1,9 @@
+// new glider(document.querySelector(',maincard'),{
+// slidesToScroll:1,
+// slidesToShow:4.5,
+// draggble:true,
+// dots :'.dots',
+// arrows : {
+//     prev : '',
+// }
+// });
